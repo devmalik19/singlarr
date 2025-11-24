f95e05151dd77e9070d1f40e0f410d76e891dc04
devmalik19 <dev.malik@outlook.com>
2025-11-24 15:12:04 +0100

    initial commit

---
+++ src/main/java/devmalik19/singlarr/SinglarrApplication.java
@@ -0,0 +1,13 @@
+package devmalik19.singlarr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglarrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinglarrApplication.class, args);
+	}
+
+}
